6df1ad9fba9145373b6ab908d7b84dd59803ad1d
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-22 14:04:37 +0200

    Used Path

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/rest/animalShelterEndpoint.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/rest/animalShelterEndpoint.java
@@ -1,4 +1,14 @@
 package at.htl.animalShelterEe.rest;
 
+import at.htl.animalShelterEe.entity.animalSpecies;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("animalShelter")
 public class animalShelterEndpoint {
+
 }
