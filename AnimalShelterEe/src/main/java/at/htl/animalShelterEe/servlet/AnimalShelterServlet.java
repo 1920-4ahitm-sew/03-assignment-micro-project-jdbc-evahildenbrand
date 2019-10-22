6254615ba2e4f3c523062c86b830b818fc3dadce
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-22 20:34:23 +0200

    Added @WebServlet and extends to AnimalShelterServlet

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
@@ -1,4 +1,8 @@
 package at.htl.animalShelterEe.servlet;
 
-public class AnimalShelterServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("animalServlet")
+public class AnimalShelterServlet extends HttpServlet {
 }
