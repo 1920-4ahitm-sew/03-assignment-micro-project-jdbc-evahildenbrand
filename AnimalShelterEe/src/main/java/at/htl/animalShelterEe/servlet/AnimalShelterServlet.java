4e8daead1740a77cb0225e67922ae18c6273f65a
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-22 20:39:57 +0200

    Added method doGet

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
@@ -2,7 +2,17 @@ package at.htl.animalShelterEe.servlet;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("animalServlet")
 public class AnimalShelterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name = req.getParameter("animal");
+        resp.getWriter().printf("<h1>Hello %s!</h1>",name);
+    }
+
 }
