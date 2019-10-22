54c149b439cb048e74af7699d52e2b36c1b62487
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-22 13:53:47 +0200

    Added Variables with Getter and Setter

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/entity/animalSpecies.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/entity/animalSpecies.java
@@ -1,4 +1,31 @@
 package at.htl.animalShelterEe.entity;
 
 public class animalSpecies {
+    private String breed;
+    private String species;
+    private String genus;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
 }
