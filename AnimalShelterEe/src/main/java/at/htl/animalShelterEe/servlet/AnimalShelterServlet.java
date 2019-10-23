33c7ed47515f05cf7fad2bca89b808e6badc5893
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-23 20:55:51 +0200

    Added printWriter and String to show

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
@@ -1,18 +1,36 @@
 package at.htl.animalShelterEe.servlet;
 
+import at.htl.animalShelterEe.database.Database;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("animalServlet")
 public class AnimalShelterServlet extends HttpServlet {
 
+    static Database db = new Database();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String name = req.getParameter("animal");
-        resp.getWriter().printf("<h1>Hello %s!</h1>",name);
+        PrintWriter printWriter = resp.getWriter();
+
+        String htmlOutput;
+
+        htmlOutput = "<html>";
+            htmlOutput += "<head>";
+                htmlOutput += "<title>Animal Species</title>";
+                htmlOutput += "<meta charset='UTF-8'>";
+            htmlOutput += "</head>";
+            htmlOutput += "<body>";
+                htmlOutput += "<h1>Animal Species</h1>";
+            htmlOutput += "</body>";
+        htmlOutput += "</html>";
+
+        printWriter.println(htmlOutput);
     }
 
 }
