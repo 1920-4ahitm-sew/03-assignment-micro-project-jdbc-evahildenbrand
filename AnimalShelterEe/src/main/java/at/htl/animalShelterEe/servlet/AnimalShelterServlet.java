0dccfc566a028b6fb910e7834acbaf512719d3d1
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-23 21:02:56 +0200

    Created table and table headers in servlet

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/servlet/AnimalShelterServlet.java
@@ -1,6 +1,7 @@
 package at.htl.animalShelterEe.servlet;
 
 import at.htl.animalShelterEe.database.Database;
+import at.htl.animalShelterEe.model.AnimalSpecies;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("animalServlet")
 public class AnimalShelterServlet extends HttpServlet {
@@ -25,6 +27,13 @@ public class AnimalShelterServlet extends HttpServlet {
             printWriter.println("</head>");
             printWriter.println("<body>");
                 printWriter.println("<h1>Animal Species</h1>");
+                printWriter.println("<table>");
+                    printWriter.println("<tr>");
+                        printWriter.println("<th>Breed</th>");
+                        printWriter.println("<th>Species</th>");
+                        printWriter.println("<th>Genus</th>");
+                    printWriter.println("</tr>");
+                printWriter.println("</table>");
             printWriter.println("</body>");
         printWriter.println("</html>");
     }
