084d4b9c31d40096567b0f0d30d4a5632273ec1d
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-23 16:52:16 +0200

    Added method openConnection to Database-Class

--- AnimalShelterEe/src/main/java/at/htl/animalShelterEe/database/Database.java
+++ AnimalShelterEe/src/main/java/at/htl/animalShelterEe/database/Database.java
@@ -1,6 +1,8 @@
 package at.htl.animalShelterEe.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
     public static final String DRIVER_STRING = "org.apache.derby.jdbc.ClientDriver";
@@ -11,5 +13,17 @@ public class Database {
     private static Connection connection;
 
     public Database() {
+    }
+
+    public static void openConnection(){
+        try {
+            Class.forName(DRIVER_STRING);
+            connection = DriverManager.getConnection(CONNECTION_STRING, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Could not open Connection to database: \n" + e.getMessage());
+        }
+
     }
 }
