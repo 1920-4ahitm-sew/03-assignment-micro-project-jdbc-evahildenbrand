c455c391c8949154b4a52ebfb7b2d13983af446b
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 19:18:56 +0200

    Created Method init

--- AnimalShelterEeJpa/src/main/java/at/htl/animalshelter/boundary/AnimalShelterEndpoint.java
+++ AnimalShelterEeJpa/src/main/java/at/htl/animalshelter/boundary/AnimalShelterEndpoint.java
@@ -1,4 +1,20 @@
 package at.htl.animalshelter.boundary;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Path;
+
+@Stateless
+@Path("/")
 public class AnimalShelterEndpoint {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("*** AnimalShelterEndpoint created! ***");
+    }
 }
