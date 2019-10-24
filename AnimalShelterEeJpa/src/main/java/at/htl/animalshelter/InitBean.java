2c77ee5a1a279f5d8895e57d1f6c110f639d274b
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 20:02:02 +0200

    Added init-Method and tearDown-Method

--- AnimalShelterEeJpa/src/main/java/at/htl/animalshelter/InitBean.java
+++ AnimalShelterEeJpa/src/main/java/at/htl/animalshelter/InitBean.java
@@ -1,4 +1,32 @@
 package at.htl.animalshelter;
 
+import at.htl.animalshelter.entity.AnimalSpecies;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
 public class InitBean {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init){
+        System.out.println("*** It works! ***");
+
+        em.persist(new AnimalSpecies("Wildkatze","Katze","Saeugetier"));
+        em.persist(new AnimalSpecies("Siamkatze","Katze","Saeugetier"));
+        em.persist(new AnimalSpecies("Golden Retriever","Hund","Saeugetier"));
+        em.persist(new AnimalSpecies("Border Collie","Hund","Saeugetier"));
+    }
+
+    public void tearDown(@Observes @Destroyed(ApplicationScoped.class) Object init){
+        
+    }
 }
