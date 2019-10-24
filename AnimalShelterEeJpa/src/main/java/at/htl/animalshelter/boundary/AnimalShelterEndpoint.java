e676c64f67bc70bdb50a44dc98d3cd4b6f1c44fa
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 19:24:18 +0200

    Created method findAnimalSpecies

--- AnimalShelterEeJpa/src/main/java/at/htl/animalshelter/boundary/AnimalShelterEndpoint.java
+++ AnimalShelterEeJpa/src/main/java/at/htl/animalshelter/boundary/AnimalShelterEndpoint.java
@@ -1,10 +1,17 @@
 package at.htl.animalshelter.boundary;
 
+import at.htl.animalshelter.entity.AnimalSpecies;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
 
 @Stateless
 @Path("/")
@@ -16,5 +23,12 @@ public class AnimalShelterEndpoint {
     @PostConstruct
     public void init(){
         System.out.println("*** AnimalShelterEndpoint created! ***");
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{breed}")
+    public AnimalSpecies findAnimalSpecies(@PathParam("breed") String breed){
+        return em.find(AnimalSpecies.class, breed);
     }
 }
