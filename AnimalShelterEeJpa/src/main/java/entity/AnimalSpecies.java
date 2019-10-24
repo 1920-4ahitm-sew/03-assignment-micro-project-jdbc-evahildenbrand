2c6ba00980b56d22b1081774626afc09fef62e9e
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 19:07:28 +0200

    Added Variables, Getter, Setter and toString()

--- AnimalShelterEeJpa/src/main/java/entity/AnimalSpecies.java
+++ AnimalShelterEeJpa/src/main/java/entity/AnimalSpecies.java
@@ -1,4 +1,48 @@
 package entity;
 
+import javax.persistence.*;
+
+@Entity
+@NamedQueries(
+        @NamedQuery(
+                name = "AnimalSpecies.findall",
+                query = "select a from AnimalSpecies a"
+        )
+)
 public class AnimalSpecies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String breed;
+    private String species;
+    private String genus;
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Id
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s %s", breed,species,genus);
+    }
 }
