ddc3c5c2fcaa77e2ea8ea3c974d787a15251f0e1
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 11:00:23 +0200

    Added method openConnection

--- AnimalShelterSe-st/src/test/java/at/htl/animalshelterse/rest/AnimalShelterIT.java
+++ AnimalShelterSe-st/src/test/java/at/htl/animalshelterse/rest/AnimalShelterIT.java
@@ -1,4 +1,31 @@
 package at.htl.animalshelterse.rest;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AnimalShelterIT {
+    public static final String DRIVER_STRING = "org.apache.derby.jdbc.ClientDriver";
+    static final String CONNECTION_STRING = "jdbc:derby://localhost:1527/db;create=true";
+    static final String USER = "app";
+    static final String PASSWORD = "app";
+
+    private static Connection connection;
+
+    @BeforeAll
+    public static void openConnection(){
+        try {
+            Class.forName(DRIVER_STRING);
+            connection = DriverManager.getConnection(CONNECTION_STRING, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Could not open Connection to database: \n" + e.getMessage());
+            System.exit(1);
+        }
+    }
+
 }
